f624b749d654f354403415eeaea3074bef1116c8
Guna-2K1 <115690371+Guna-2K1@users.noreply.github.com>
2026-03-20 20:00:26 +0530

    Update tomcat.sh

--- tomcat.sh
+++ tomcat.sh
@@ -1,11 +1,29 @@
-amazon-linux-extras install java-openjdk11 -y
+# Install latest Java (Corretto 21)
+sudo yum install java-21-amazon-corretto -y
+
+# Set JAVA_HOME
+export JAVA_HOME=/usr/lib/jvm/java-21-amazon-corretto.x86_64
+export PATH=$JAVA_HOME/bin:$PATH
+
+# Verify Java
+java -version
+
+# Download Tomcat
 wget https://dlcdn.apache.org/tomcat/tomcat-9/v9.0.116/bin/apache-tomcat-9.0.116.tar.gz
+
+# Extract
 tar -zxvf apache-tomcat-9.0.116.tar.gz
+
+# Configure users (same as your script)
 sed -i '56  a\<role rolename="manager-gui"/>' apache-tomcat-9.0.116/conf/tomcat-users.xml
 sed -i '57  a\<role rolename="manager-script"/>' apache-tomcat-9.0.116/conf/tomcat-users.xml
 sed -i '58  a\<user username="tomcat" password="admin@123" roles="manager-gui, manager-script"/>' apache-tomcat-9.0.116/conf/tomcat-users.xml
 sed -i '59  a\</tomcat-users>' apache-tomcat-9.0.116/conf/tomcat-users.xml
 sed -i '56d' apache-tomcat-9.0.116/conf/tomcat-users.xml
+
+# Allow manager access (same as your script)
 sed -i '21d' apache-tomcat-9.0.116/webapps/manager/META-INF/context.xml
-sed -i '22d'  apache-tomcat-9.0.116/webapps/manager/META-INF/context.xml
+sed -i '22d' apache-tomcat-9.0.116/webapps/manager/META-INF/context.xml
+
+# Start Tomcat
 sh apache-tomcat-9.0.116/bin/startup.sh
